7c1944e2e0bb2a439cf668d88fc8cb7f12be8450
TRobster <trevorro@uoregon.edu>
2025-11-21 16:39:40 -0800

    Works

--- embeddings.py
+++ embeddings.py
@@ -1,5 +1,6 @@
 import numpy as np
 from sentence_transformers import SentenceTransformer
+import torch
 
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
@@ -8,3 +9,88 @@ def embed(text: str) -> np.ndarray:
     return vec.astype("float32")
 
 
+
+"""
+Author: Trevor Robbins
+File Purpose: Embedding of sentences using 'SentenceTransformer" to vectorize data in backend
+
+Goals: Understand basic linear algebraeic concepts such as dot products, euclidean normality, and cosine similarity
+       of two different vectors 
+       
+
+"""
+
+
+
+
+sentences = [
+    "A neural network can learn patterns.",
+    "A neural network is capable of learning patterns."
+]
+
+embs = model.encode(sentences,convert_to_tensor=True)
+
+
+"""testVAL1 = np.random.rand(1024)
+compare1 = np.random.rand(1024)
+
+testVAL2 = np.random.rand(512)
+compare2 = np.random.rand(512)
+
+testVAL3 = np.random.rand(256)
+compare3 = np.random.rand(256)
+
+testVAL4 = np.random.rand(128)
+compare4 = np.random.rand(128)
+
+x = cosine_sim(testVAL1, compare1)
+y = cosine_sim(testVAL2, compare2)
+z= cosine_sim(testVAL3, compare3)
+g = cosine_sim(testVAL4, compare4)
+
+print(x)
+print(y)
+print(z)
+print(g)
+"""
+
+
+
+
+#def vectorCompare(x, y):
+    
+""""
+def dot(x, y):
+    return np.dot(x, y)
+"""
+
+def eucilidean_norm(x):
+    """
+    Mathematical formula in pseudocode:
+
+    sumSquare = 0 
+    for i = 0 to n:
+        sumSquare = sumSquare + (i*i) 
+    
+    return np.sqrt(sumSquare)
+        
+        
+    """
+    return np.linalg.norm(x)
+
+
+def cosine_sim(x, y):
+    """
+    z = (a * b)/
+        |a|*|b|
+
+    """
+    return np.dot(x, y)/(np.linalg.norm(x) * np.linalg.norm(y))
+
+
+
+#print("Shape:", embs.shape)
+#print("First vector (first 5 dims):", embs[0][:])
+print(torch.cosine_similarity(embs[0], embs[1], dim = 0))
+
+#print(torch.cuda.is_available())  # True = using GPU
